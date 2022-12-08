10211e6442ca99efbd984f3299893e2f7d0465ee
Kashira2 <nekit.egorov@icloud.com>
2022-12-08 09:45:09 +0300

    Creat start array

--- dz/Program.cs
+++ dz/Program.cs
@@ -1,1 +1,36 @@
-﻿
+﻿string[] Array(string[] array)
+{
+    Console.WriteLine("Введите первый элемент массива: ");
+    string a = Console.ReadLine();
+    Console.WriteLine("Введите второй элемент массива: ");
+    string b = Console.ReadLine();
+    Console.WriteLine("Введите третий элемент массива: ");
+    string c = Console.ReadLine();
+    Console.WriteLine("Введите четвертый элемент массива: ");
+    string d = Console.ReadLine();
+
+    array[0] = a;
+    array[1] = b;  
+    array[2] = c;
+    array[3] = d;
+
+    return array;
+}
+
+void PrintArray(string[] array)
+{
+    foreach (string item in array)
+    {
+        Console.Write($"{item}, ");
+
+    }
+}
+
+
+
+string[] array = new string[4];
+
+Array(array);
+Console.WriteLine();
+PrintArray(array);
+Console.WriteLine();
